4a2e5874fc0c6e16f4f202c17dd25673b7353c06
palmercluff <palmercluff@gmail.com>
2017-04-02 06:45:37 +0000

    Resolved issue #2. Also added comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tor-checker
-A server-based program that is able to determine whether or not a client visiting your website is on the Tor browser.
+A server-based program that is able to determine whether or not a client visiting your website is on the Tor browser. This application may be useful if you are wanting to block Tor users from accessing the core of your website or web application. Tor ruins web analytics because the Tor network bounces users across the globe therfore making analytics less acurate. Although this solution is not foolproof, it is a good addition to incoorporate into your website or web application.
 
 - To put on your server, run the following on your Debian (or similar) web server:
   - `wget https://github.com/palmercluff/tor-checker/archive/master.zip`
--- tor_checker.php
+++ tor_checker.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
   <body>
-    
+
+    <!-- You can place this script on any PHP page you want -->
     <?php
        $is_tor_client = False;
        $client_ip=$_SERVER['REMOTE_ADDR'];
@@ -9,20 +10,23 @@
        
        $myfile = fopen('ip_addresses', 'r') or die('Unable to open file!');
        while(!feof($myfile)) {
-       $tor_ip = fgets($myfile);
-       $tor_ip = trim($tor_ip);
-       if ($client_ip == $tor_ip)
-         $is_tor_client = True;
+         $tor_ip = fgets($myfile);
+         $tor_ip = trim($tor_ip);
+         if ($client_ip == $tor_ip) {
+           $is_tor_client = True;
+         }
        }
        fclose($myfile);
        
-       if ($is_tor_client){
+       if ($is_tor_client) {
+         // Do whatever you want here if they are on the Tor network
          echo 'You are on the Tor network';
        }
        else {
+         // Do whatever you want here if they are not on the Tor network
          header('Location: index.html');
        }
-       ?>
+    ?>
     
   </body>
 </html>
